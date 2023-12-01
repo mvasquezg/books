c6a2d70c404af91ed0d2a66e2dc57916dc318b7e
mvasquezg <ing.mvasquezg@gmail.com>
2023-12-01 17:55:52 -0600

    Agregando comentarios al programa 001 en depreciation

--- FundamentosDeLaProgramacionLuisJoyanes4taEdicion/chapter02/JAVASCRIPT/js/depreciation.js
+++ FundamentosDeLaProgramacionLuisJoyanes4taEdicion/chapter02/JAVASCRIPT/js/depreciation.js
@@ -1,3 +1,15 @@
+/*Libro:Fundamentos de Programación, Algoritmos, estructura de datos y objetos 4ta edición. 
+  Capitulo 02.-Metodología de la programación y desarrollo de software.
+  Problema 01.Se desea obtener una tabla con las depreciaciones acumuladas y los valores reales de cada año, de un automóvil
+  comprado por 20.000 euros en el año 2005, durante los seis años siguientes suponiendo un valor de recuperación o
+  rescate de 2.000. Realizar el análisis del problema, conociendo la fórmula de la depreciación anual constante D
+  para cada año de vida útil.
+
+  D = coste – valor de recuperación/vida útil
+
+  D = (20,000 – 2,000)/6 = 18,000/6 = 3,000  
+*/
+
     /**variables que almacenan los valores de los inputs */
     var idCostVehicle=0;        //Costo del vehiculo
     var idSalvageValue=0;       //Costo de recuperación
