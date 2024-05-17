6e11dcece559c74fdb4a26687b2f8b08d14ea0df
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-17 15:55:23 -0600

    Programa08.-Imprimir 100 veces feliz cumpleaños. Libro Introduccion a la programacion con java, capitulo 2

---
+++ IntroduccionAlaProgramacionConJavaPrimeraEdicion/chapter02/PrintHappyBirthday.java
@@ -0,0 +1,12 @@
+/** 
+ *Libro: Introducción a la programación con Java primera edición
+ *Capitulo 2: Algoritmos y diseños 
+ *Programa 8: Imprimir 100 veces feliz cumpleaños
+*/
+public class PrintHappyBirthday{
+    public static void main(String[] args){
+        for(int i=1; i<=100; i++){
+            System.out.println(i+".-Feliz Cumpleaños");
+        }
+    }
+}
