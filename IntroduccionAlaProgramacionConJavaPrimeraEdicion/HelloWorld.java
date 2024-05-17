b1d4c4e8f8111ed27c01f910d0e713fd9031ec7d
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-16 22:06:04 -0600

    Programa01.-Hola mundo. Libro Introduccion a la programacion con java, capitulo 1

---
+++ IntroduccionAlaProgramacionConJavaPrimeraEdicion/HelloWorld.java
@@ -0,0 +1,10 @@
+/** 
+ *Libro: Introducción a la programación con Java primera edición
+ *Capitulo 1: Introducción a las computadoras y la programación 
+ *Programa 1: Hola Mundo
+*/
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }   
+}
