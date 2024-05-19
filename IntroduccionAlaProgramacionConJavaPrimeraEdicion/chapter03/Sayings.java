ebb652961040970d56dc3d0b1f9e4256e335193f
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-19 09:46:00 -0600

    Programa02.-Imprimir varias lineas en consola. Libro Introduccion a la programacion con java, capitulo 3

---
+++ IntroduccionAlaProgramacionConJavaPrimeraEdicion/chapter03/Sayings.java
@@ -0,0 +1,13 @@
+/** 
+ *Libro: Introducción a la programación con Java primera edición
+ *Capitulo 3: Fundamentos de Java 
+ *Programa 1: Imprimir  dichos
+*/
+
+public class Sayings{
+    public static void main(String[] args){
+        System.out.println("El futuro no es lo que solia ser.");
+        System.out.println("Siempre recuerda que tu eres unico, exactamente igual que los demas");
+        System.out.println("Si no eres parte de la solución,"+" eres parte del problema");
+    }
+}
