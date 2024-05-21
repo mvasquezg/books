d11486b278f94a0dfab832e0d0ad5a3b3475ff0c
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-21 11:01:43 -0600

    Programa08.-Uso del metodo length. Libro Introduccion a la programacion con java, capitulo 3

---
+++ IntroduccionAlaProgramacionConJavaPrimeraEdicion/chapter03/MethodLength.java
@@ -0,0 +1,14 @@
+/** 
+ *Libro: Introducción a la programación con Java primera edición
+ *Capitulo 3: Fundamentos de Java 
+ *Programa 7: Uso del metodo length*/
+
+public class MethodLength{
+    public static void main(String[] args){
+        String s1="hola";
+        String s2="";
+
+        System.out.println(s1 +": tiene " + s1.length()+" caracteres.");
+        System.out.println(s2 +": tiene " + s2.length()+" caracteres.");
+    }
+}
