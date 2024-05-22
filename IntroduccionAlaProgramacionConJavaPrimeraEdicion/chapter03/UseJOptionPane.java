954b59951d8b185b2516ab8e7d5164337c75210b
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-21 20:57:59 -0600

    Programa13.-Uso de la clase JOptionPane. Libro Introduccion a la programacion con java, capitulo 3

---
+++ IntroduccionAlaProgramacionConJavaPrimeraEdicion/chapter03/UseJOptionPane.java
@@ -0,0 +1,14 @@
+/** 
+ *Libro: Introducción a la programación con Java primera edición
+ *Capitulo 3: Fundamentos de Java 
+ *Programa 13: Uso de la clase JOptionPane
+ */
+import javax.swing.JOptionPane;
+
+public class UseJOptionPane{
+    public static void main(String[] args){
+        
+        JOptionPane.showMessageDialog(null, "Antes de comenzar la instalación, cierre todas las aplicaciones");
+        
+    }
+}
