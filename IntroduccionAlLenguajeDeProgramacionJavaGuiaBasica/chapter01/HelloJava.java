d351337e9c4d8d38b6794f0820cc63c56ac67755
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-23 13:59:55 -0600

    Programa01.-Mensaje en consola. Libro Introducción al lenguaje de programación Java. Una guía básica, capitulo 1

---
+++ IntroduccionAlLenguajeDeProgramacionJavaGuiaBasica/chapter01/HelloJava.java
@@ -0,0 +1,10 @@
+/** 
+ *Libro: Introducción al lenguaje de programación Java. Una guía básica.
+ *Capitulo 1: Sentencias de control
+ *Programa 1: Mensaje en consola.
+ */
+public class HelloJava{
+    public static void main(String[] args){
+        System.out.println("Hola Java");
+    }
+}
