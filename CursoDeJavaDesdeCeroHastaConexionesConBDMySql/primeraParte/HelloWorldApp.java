20173c6cafaa319970112630aae4c14fb5c2dbea
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-30 12:56:05 -0600

    Programa02.-Mensaje en pantalla orientado a objetos. Libro Curso de Java desde cero Hasta Conexiones con BD MYSQL, primera parte

---
+++ CursoDeJavaDesdeCeroHastaConexionesConBDMySql/primeraParte/HelloWorldApp.java
@@ -0,0 +1,14 @@
+/** 
+ *Libro: Curso de Java desde Cero hasta Conexiones con Base de Datos MYSQL
+ *Primera parte: Iniciación al lenguaje Java
+ *Programa 2: Mensaje en consola hola mundo orientado a objetos
+ */
+
+
+public class HelloWorldApp{
+    public static void main(String[] args){
+        HelloWorldOO message=new HelloWorldOO();
+
+        message.showGreeting();
+    }
+}
