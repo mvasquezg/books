f51c9fbab5bd1ce4fcce5760b1f2350ad897ec92
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-30 16:53:50 -0600

    Programa03.-Mensaje personalizado usando JOptionPane. Libro Curso de Java desde cero Hasta Conexiones con BD MYSQL, primera parte

---
+++ CursoDeJavaDesdeCeroHastaConexionesConBDMySql/primeraParte/DemoJOptionPaneApp.java
@@ -0,0 +1,12 @@
+/** 
+ *Libro: Curso de Java desde Cero hasta Conexiones con Base de Datos MYSQL
+ *Primera parte: Iniciación al lenguaje Java
+ *Programa 3: Demo JOptionPane
+ */
+
+public class DemoJOptionPaneApp{
+    public static void main(String[] args){
+        DemoJOptionPane hellloGreting=new DemoJOptionPane();
+    }
+}
+ 
