160e4944802e3359def7ae5c4841838594e2da6f
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-31 07:23:33 -0600

    Programa05.-Operador logico OR. Libro Curso de Java desde cero Hasta Conexiones con BD MYSQL, primera parte

---
+++ CursoDeJavaDesdeCeroHastaConexionesConBDMySql/primeraParte/DemoIfElseOrApp.java
@@ -0,0 +1,12 @@
+/** 
+ *Libro: Curso de Java desde Cero hasta Conexiones con Base de Datos MYSQL
+ *Primera parte: Iniciación al lenguaje Java
+ *Programa 5: Demo if-else Or
+ */
+
+public class DemoIfElseOrApp{
+    public static void main(String[] args){
+        DemoIfElseOr hellloGreting=new DemoIfElseOr();
+    }
+}
+ 
