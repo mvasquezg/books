e0166360736cda360c023723789df02a5fe66ca1
mvasquezg <ing.mvasquezg@gmail.com>
2024-05-31 07:30:49 -0600

    Programa06.-Bucle While. Libro Curso de Java desde cero Hasta Conexiones con BD MYSQL, primera parte

---
+++ CursoDeJavaDesdeCeroHastaConexionesConBDMySql/primeraParte/DemoWhileApp.java
@@ -0,0 +1,12 @@
+/** 
+ *Libro: Curso de Java desde Cero hasta Conexiones con Base de Datos MYSQL
+ *Primera parte: Iniciación al lenguaje Java
+ *Programa 6: Demo while
+ */
+
+public class DemoWhileApp{
+    public static void main(String[] args){
+        DemoWhile hellloGreting=new DemoWhile();
+    }
+}
+ 
