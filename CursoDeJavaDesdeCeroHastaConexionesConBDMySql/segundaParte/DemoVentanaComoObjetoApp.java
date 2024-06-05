65e9db513c7ba12038da52f0391e27214b363b02
mvasquezg <ing.mvasquezg@gmail.com>
2024-06-05 13:28:33 -0600

    Programa02.-Demo ventana como objeto. Curso de Java desde Cero hasta Conexiones con Base de Datos MYSQL. Una guía básica, segunda parte

---
+++ CursoDeJavaDesdeCeroHastaConexionesConBDMySql/segundaParte/DemoVentanaComoObjetoApp.java
@@ -0,0 +1,14 @@
+/** 
+ *Libro: Curso de Java desde Cero hasta Conexiones con Base de Datos MYSQL
+ *Segunda parte: Aplicacion de Ejemplo
+ *Programa 2: Demo Ventana como Objeto
+ */
+
+public class DemoVentanaComoObjetoApp{
+
+    public static void main(String[] args){
+        DemoVentanaComoObjeto demo=new DemoVentanaComoObjeto();
+    }
+
+
+}
