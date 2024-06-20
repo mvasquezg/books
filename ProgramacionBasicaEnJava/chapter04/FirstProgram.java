b44c216cfd7189df4d039fa4cff93d50e1abff2b
mvasquezg <ing.mvasquezg@gmail.com>
2024-06-20 16:05:30 -0600

    Programa01.-Mensaje en consola. Programacion basica en Java, capitulo 4

---
+++ ProgramacionBasicaEnJava/chapter04/FirstProgram.java
@@ -0,0 +1,10 @@
+/** 
+ *Libro: Curso de Java desde Cero hasta Conexiones con Base de Datos MYSQL
+ *Capitulo 4: Escritura de programas en Java
+ *Programa 1: Primer pŕograma
+ */
+public class FirstProgram{
+    public static void main(String[] args){
+        System.out.println("Mi primer programa en Java\n");     
+    }
+}
