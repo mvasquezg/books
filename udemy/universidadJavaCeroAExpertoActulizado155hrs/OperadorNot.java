ec3e4458a64cff90593d8249f6678f30dde6eca2
mavg <ing.mvasquezg@gmail.com>
2026-03-01 03:11:43 -0600

    Operador Not

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/OperadorNot.java
@@ -0,0 +1,13 @@
+public class OperadorNot{
+    public static void main(String[] args){
+        
+        System.out.println("*** Operador Not ***");
+
+        boolean a=false;
+        
+        //Not (invierte el valor logico)
+        var result=!a;
+
+        System.out.println("Resultado: "+result);
+    }
+}
