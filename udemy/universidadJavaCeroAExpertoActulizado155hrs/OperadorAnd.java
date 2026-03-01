248d1be22d1c3da569145c25cc7607925fa4ed76
mavg <ing.mvasquezg@gmail.com>
2026-03-01 03:00:41 -0600

    Operador And

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/OperadorAnd.java
@@ -0,0 +1,10 @@
+public class OperadorAnd{
+    public static void main(String[] args){
+        boolean a=true, b=true;
+
+        //and (regresa true si ambos valores son true)
+        var result=a&&b;
+
+        System.out.println("Resultado: "+result);
+    }
+}
