0352e5a89019740210bb6adb99d745b25dc46ddf
mavg <ing.mvasquezg@gmail.com>
2026-03-04 19:58:17 -0600

    Uso de la sentencia IF-Else

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/SentenciaIfElse.java
@@ -0,0 +1,12 @@
+public class SentenciaIfElse{
+    public static void main(String[] args){
+        //Uso de la sentencia if
+        var edad=17;
+
+        if(edad>=18){
+            System.out.println("Eres mayor de edad.");
+        }else{
+            System.out.println("Eres menor de edad.");
+        }
+    }
+}
