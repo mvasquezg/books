cdee80e5dcbabff7f48970a8d1ef7f870fa76f41
mavg <ing.mvasquezg@gmail.com>
2026-03-04 21:26:18 -0600

    Uso de la Sentencia If-Else If-Else, determinando si un numero es positivo, negativo o cero

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/PositiveNumber.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class PositiveNumber{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int number=0;
+
+        System.out.print("Ingresa un numero: ");
+        number=in.nextInt();
+
+        if (number>0) {
+            System.out.println("El numero es positivo");
+        }else if (number<0) {
+            System.out.println("El numero es negativo");
+        }else{
+            System.out.println("El numero es 0");
+        }
+    }
+}
