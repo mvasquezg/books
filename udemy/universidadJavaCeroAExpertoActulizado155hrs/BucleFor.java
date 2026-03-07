3b02c9be69b2c7868d5ac05aa3f2a4b19a18b732
mavg <ing.mvasquezg@gmail.com>
2026-03-07 00:49:09 -0600

    Uso del bucle for

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/BucleFor.java
@@ -0,0 +1,10 @@
+public class BucleFor{
+    public static void main(String[] args){
+        System.out.println("*** Ciclo For***");
+        
+        for(var i=1; i<=3; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println("\n");
+    }
+}
