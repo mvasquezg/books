fc570dd340b0ed326aa2994f2f4e69a46757c99c
mavg <ing.mvasquezg@gmail.com>
2026-03-06 21:08:06 -0600

    Uso Do-While, numeros inversos

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/NumInverso.java
@@ -0,0 +1,14 @@
+public class NumInverso{
+    public static void main(String[] args){
+        System.out.println("*** Numero Inverso ***");
+
+        var i=3;
+        do{
+            System.out.print(i+" ");
+            i--;
+        }while(i>=1);
+        System.out.println();
+    }
+
+    
+}
