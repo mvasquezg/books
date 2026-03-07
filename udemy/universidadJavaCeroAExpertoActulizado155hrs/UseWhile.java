1a7feb0594a000fd528c66104aa53bf00b409082
mavg <ing.mvasquezg@gmail.com>
2026-03-06 20:10:03 -0600

    Uso de la sentacia While

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/UseWhile.java
@@ -0,0 +1,12 @@
+public class UseWhile{
+    public static void main(String[] args){
+        var i=1;
+
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
+
