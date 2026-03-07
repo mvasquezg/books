91123d95f0c638850c2dbfc3807c623d53c622bb
mavg <ing.mvasquezg@gmail.com>
2026-03-06 20:50:47 -0600

    Uso del bucle Do-While

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/UseDoWhile.java
@@ -0,0 +1,10 @@
+public class UseDoWhile{
+    public static void main(String[] args){
+        var i=1;
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while(i<=3);
+        System.out.println("\n");
+    }
+}
