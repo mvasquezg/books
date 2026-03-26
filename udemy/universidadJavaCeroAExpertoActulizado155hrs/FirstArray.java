a312f88bc618438f0a6e56d2c8a350f873d87e49
mavg <ing.mvasquezg@gmail.com>
2026-03-25 20:16:34 -0600

    Declaracion de arreglos

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/FirstArray.java
@@ -0,0 +1,19 @@
+public class FirstArray{
+    public static void main(){
+        //1.-Declaracion de arreglos
+        int[] arrayInt;
+        
+        //2.-Inicializar arreglos
+        arrayInt=new int[3];
+
+        //3.-declarar e iniciarlizar un arreglo
+        //int[] arrayNumbers=new int[2];
+
+        var arrayNumbers=new int[2];
+        
+
+
+
+
+    }
+}
