9891cab26eb7648ce4109bc781f69c14a9ec4a66
mavg <ing.mvasquezg@gmail.com>
2026-03-25 20:23:17 -0600

    Modifcando los valores del arreglo

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/ModifiedArray.java
@@ -0,0 +1,14 @@
+public class ModifiedArray{
+    public static void main(String[] args){
+
+        //Definir arreglos
+        var arrayInt=new int[5];
+        
+        //Moidficar elementos
+        arrayInt[0]=13;
+        arrayInt[1]=21;
+        arrayInt[4]=62;
+
+
+    }
+}
