c51b1ec4c2cc45c8f9ad52a6105e4b850c4972ee
mavg <ing.mvasquezg@gmail.com>
2026-03-25 20:38:40 -0600

    Imprimiendo los valores del arreglo

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/PrintArrayvalue.java
@@ -0,0 +1,18 @@
+public class PrintArrayvalue{
+    public static void main(String[] args){
+        //Definir arreglos
+        var arrayInt=new int[5];
+        
+        //Moidficar elementos
+        arrayInt[0]=13;
+        arrayInt[1]=21;
+        arrayInt[4]=62;
+
+          //Leyendo los elementos de una Arreglo
+        System.out.println("array[0]: "+arrayInt[0]);
+        System.out.println("array[1]: "+arrayInt[1]);
+        System.out.println("array[2]: "+arrayInt[2]);
+        System.out.println("array[3]: "+arrayInt[3]);
+        System.out.println("array[4]: "+arrayInt[4]);
+    }
+}
