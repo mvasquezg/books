19df1e9b5e1d399d97444805b7ef658219b71979
mavg <ing.mvasquezg@gmail.com>
2026-03-25 23:06:57 -0600

    Se muestra la simpicidad del arreglo, y tambien se muestra como se debn imprimir los valores del arreglo, sin emebargo si intentas imprimir el arreglo como tal esto se vera algo extraño en ña impresion, entonces debes ucar un loop o blucle o ciclo como lo llames en tu pais

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/SimplifiedArraySintax.java
@@ -0,0 +1,16 @@
+public class SimplifiedArraySintax{
+    public static void main(String[] args){
+        //arreglo
+        //int[] arrayInt= {100, 200, 300, 400 500}
+        
+        var arrayInt= new int[]{100, 200, 300, 400, 500};
+
+        System.out.println("Valor [0]: "+arrayInt[0]);
+        System.out.println("Valor [1]: "+arrayInt[1]);
+        System.out.println("Valor [2]: "+arrayInt[2]);
+        System.out.println("Valor [3]: "+arrayInt[3]);
+        System.out.println("Valor [4]: "+arrayInt[4]);
+
+        System.out.println("imprimir el arreglo: "+arrayInt);
+    }
+}
