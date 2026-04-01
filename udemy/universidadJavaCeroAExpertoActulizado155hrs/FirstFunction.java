9bf127f58712b4291653f695946c4c0f18f5ba82
mavg <ing.mvasquezg@gmail.com>
2026-04-01 04:09:06 -0600

    Primera funcion en java

---
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/FirstFunction.java
@@ -0,0 +1,10 @@
+public class FirstFunction {
+
+    static void saludar(String message){
+        System.out.println("Mensaje: "+message);
+    }
+
+    public static void main(String[] args) {
+        saludar("Hola desde Java");
+    }
+}
