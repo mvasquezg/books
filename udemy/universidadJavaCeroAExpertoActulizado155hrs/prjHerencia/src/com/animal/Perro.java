37b4f3d453e90d082b5263f33c7adeafaf9f953e
mavg <ing.mvasquezg@gmail.com>
2026-04-14 06:20:33 -0600

    uso de la palabra super

--- udemy/universidadJavaCeroAExpertoActulizado155hrs/prjHerencia/src/com/animal/Perro.java
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/prjHerencia/src/com/animal/Perro.java
@@ -8,5 +8,7 @@ public class Perro extends Animal {
 
     public void dormir(){
         System.out.println("Duermo 15 horas al dia");
+        System.out.println("Clase Padre: ");
+        super.dormir();
     }
 }
