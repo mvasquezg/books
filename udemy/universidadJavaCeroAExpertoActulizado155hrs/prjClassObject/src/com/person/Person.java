072306ba8c3dbf96816bbd67d467ac2ecff33aa6
mavg <ing.mvasquezg@gmail.com>
2026-04-16 08:42:28 -0600

    Usando variable statica y dando uso

--- udemy/universidadJavaCeroAExpertoActulizado155hrs/prjClassObject/src/com/person/Person.java
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/prjClassObject/src/com/person/Person.java
@@ -1,14 +1,19 @@
 package prjClassObject.src.com.person;
-
+              
 public class Person{
+    public static int countPerson = 0;
+
     private String namePerson;
     private String lastNamePerson;
 
     public Person(){}
 
     public Person(String namePerson, String lastNamePerson){
+        
         this.namePerson=namePerson;
         this.lastNamePerson=lastNamePerson;
+        //incremenetar el atributo estatico
+        Person.countPerson++;
     }
 
     public void setNamePerson(String namePerson){
--- udemy/universidadJavaCeroAExpertoActulizado155hrs/prjClassObject/src/com/personapp/PersonApp.java
+++ udemy/universidadJavaCeroAExpertoActulizado155hrs/prjClassObject/src/com/personapp/PersonApp.java
@@ -1,12 +1,19 @@
 package prjClassObject.src.com.personapp;
+        
+import  prjClassObject.src.com.person.Person;
 
-import prjClassObject.src.com.person.Person;
 public class PersonApp {
     public static void main(String[] args) {
-        System.out.println("*** Hereando de la clase Object ***");
-        var object1=new Person("Layda", "Acosta");
+        System.out.println("*** Contexto estatico ***");
 
+        System.out.println("Variable estatica: "+Person.countPerson);
+        var object1=new Person("Layda", "Acosta");
+        System.out.println(object1);    
+        System.out.println(Person.countPerson);
+        System.out.println();
         //System.out.println(object1.toString());
-        System.out.println(object1);
+        var object2=new Person("Miguel", "Vasquez");
+        System.out.println(object2);
+        System.out.println(Person.countPerson);
     }   
 }
