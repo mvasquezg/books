0727cd182bed28607a78250035346b33f17c903d
mavg <ing.mvasquezg@gmail.com>
2026-06-22 01:27:35 -0600

    Clase Principal se elimino espacio en blanco

--- ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/app/HotelReservationApp.java
+++ ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/app/HotelReservationApp.java
@@ -7,7 +7,6 @@ import com.reservation.hotel.view.frame.RoomHotelFrame;
 public class HotelReservationApp {
 	public static void main(String[] args) {
 		RoomHotelFrame frmRoom=new RoomHotelFrame();
-		
 		frmRoom.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
