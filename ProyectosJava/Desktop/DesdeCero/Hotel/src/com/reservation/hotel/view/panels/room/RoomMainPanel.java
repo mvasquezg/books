069e1cedfd0d932fe50e2f96944e8c13baa44c78
mavg <ing.mvasquezg@gmail.com>
2026-06-22 01:55:26 -0600

    Se agrego el SerialVersionUID, el objeto showDatapanel se creo en el contructor, y se crearon los metodos setter y getter de showDaraPanel y InsertDatapanel

--- ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/view/panels/room/RoomMainPanel.java
+++ ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/view/panels/room/RoomMainPanel.java
@@ -5,14 +5,33 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 public class RoomMainPanel extends JPanel{
+	
+	private static final long serialVersionUID = 2578356520289406250L;
+	
 	private InsertDataPanel insertDataPanel=new InsertDataPanel(); 
-	private ShowDataPanel showDataPanel=new ShowDataPanel();
+	private ShowDataPanel showDataPanel;
 	
 	public RoomMainPanel() {
 		setLayout(new GridLayout(1, 2, 10, 10));//Divide el panel principal en dos a un tamaño
-		//add(insertDataPanel, BorderLayout.WEST);
-		//add(showDataPanel, BorderLayout.EAST);
+		
 		add(insertDataPanel);
+		showDataPanel=new ShowDataPanel(insertDataPanel);
 		add(showDataPanel);
-	};
+	}
+
+	public InsertDataPanel getInsertDataPanel() {
+		return insertDataPanel;
+	}
+
+	public void setInsertDataPanel(InsertDataPanel insertDataPanel) {
+		this.insertDataPanel = insertDataPanel;
+	}
+
+	public ShowDataPanel getShowDataPanel() {
+		return showDataPanel;
+	}
+
+	public void setShowDataPanel(ShowDataPanel showDataPanel) {
+		this.showDataPanel = showDataPanel;
+	}	
 }
