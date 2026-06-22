81120a2192ee8e320c0af409b26ee96e566404e4
mavg <ing.mvasquezg@gmail.com>
2026-06-22 02:08:03 -0600

    se eliminaron espacion en blanco y tambien se actulizaron los metodos oyentes para pasar por parametros los botones de este panel, tambien se crearon los metodos setter y getter para cada boton

--- ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/view/panels/room/ButtonRoomPanel.java
+++ ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/view/panels/room/ButtonRoomPanel.java
@@ -1,35 +1,69 @@
 package com.reservation.hotel.view.panels.room;
 
 import java.awt.Color;
-import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import com.reservation.hotel.controller.ButtonFunctions;
-import com.reservation.hotel.controller.QuerysRoom;
+import com.reservation.hotel.controller.ButtonRoomFunctionsController;
 
 public class ButtonRoomPanel extends JPanel{
+	private static final long serialVersionUID = -1365304798766586098L;
+
 	private JButton jBtSave=new JButton("Guardar");
 	private JButton jBtCancel=new JButton("Cancelar");
 	private JButton jBtNew=new JButton("Nuevo");
 	
 	private InputRoomPanel inputPanel=new InputRoomPanel();
 	
-	private String actionBtn="Guardar";
+	public ButtonRoomPanel() {
+		
+	}
 	
 	public ButtonRoomPanel(InputRoomPanel inputPanel) {
 		//Aplicando transparencia
 		setOpaque(false);
 		setBackground(new Color(0, 0,  0, 0));
 		
-		jBtNew.addActionListener(new ButtonFunctions(inputPanel));
+		jBtNew.addActionListener(new ButtonRoomFunctionsController(inputPanel, jBtNew, jBtSave, jBtCancel));
 		
 		add(jBtNew);
 		
-		jBtSave.addActionListener(new QuerysRoom(inputPanel));
+		jBtSave.addActionListener(new ButtonRoomFunctionsController(inputPanel, jBtSave, jBtSave, jBtCancel));
 		add(jBtSave);
 		
 		add(jBtCancel);
 	}
+
+	public JButton getjBtSave() {
+		return jBtSave;
+	}
+
+	public void setjBtSave(JButton jBtSave) {
+		this.jBtSave = jBtSave;
+	}
+
+	public JButton getjBtCancel() {
+		return jBtCancel;
+	}
+
+	public void setjBtCancel(JButton jBtCancel) {
+		this.jBtCancel = jBtCancel;
+	}
+
+	public JButton getjBtNew() {
+		return jBtNew;
+	}
+
+	public void setjBtNew(JButton jBtNew) {
+		this.jBtNew = jBtNew;
+	}
+
+	public InputRoomPanel getInputPanel() {
+		return inputPanel;
+	}
+
+	public void setInputPanel(InputRoomPanel inputPanel) {
+		this.inputPanel = inputPanel;
+	}	
 }
