efb240f5cb554a187a07e3faecfd3d0a01199720
mavg <ing.mvasquezg@gmail.com>
2026-06-23 16:20:10 -0600

    Se queda la clase QuerysRoom, como una clase que hace los queruys a la bd y se quita como un oyente a evento

--- ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/controller/QuerysRoom.java
+++ ProyectosJava/Desktop/DesdeCero/Hotel/src/com/reservation/hotel/controller/QuerysRoom.java
@@ -1,7 +1,7 @@
 package com.reservation.hotel.controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,8 +12,9 @@ import javax.swing.table.DefaultTableModel;
 
 import com.reservation.hotel.model.beans.RoomBean;
 import com.reservation.hotel.view.panels.room.InputRoomPanel;
+//import com.reservation.hotel.view.panels.room.InsertDataPanel;
 
-public class QuerysRoom implements ActionListener{
+public class QuerysRoom {
 	
 	private ConnectionToDB conect=new ConnectionToDB();
 	private Connection cn=conect.getConnectionBD();
@@ -21,80 +22,7 @@ public class QuerysRoom implements ActionListener{
 	private String querySql="";
 	private Integer registerTotal;
 	
-	private InputRoomPanel panelInput;
-	private RoomBean room=new RoomBean();
-	
 	public QuerysRoom() {
-		
-	}
-	
-	public QuerysRoom(InputRoomPanel panelInput) {
-		this.panelInput=panelInput;
-	}
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		if(!panelInput.getjTxtRoomNumber().getText().equals("")) {
-			room.setRoomNumber(panelInput.getjTxtRoomNumber().getText());
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"El campo numero esta vacio");
-		}
-		
-		if(!panelInput.getjCbxFloor().getSelectedItem().equals("Seleccione un piso")) {
-			room.setFloor((String) panelInput.getjCbxFloor().getSelectedItem());
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"No ha seleccionado un piso.");
-		}
-		
-		if(!panelInput.getjTxtADescription().getText().equals("")) {
-			room.setDescription(panelInput.getjTxtADescription().getText());
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"El campo descripcion esta vacio");
-		}
-		
-		if(!panelInput.getjTxtACharacteristics().getText().equals("")) {
-			room.setCharacteristics(panelInput.getjTxtACharacteristics().getText());
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"El campo caracteristca esta vacio");
-		}
-		
-		if(!panelInput.getjTxtRoomNumber().getText().equals("")) {
-			room.setRoomNumber(panelInput.getjTxtRoomNumber().getText());
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"El campo numero esta vacio");
-		}
-		
-		if(!panelInput.getjTxtPriceNight().getText().equals("")) {
-			room.setPriceNight((Double.parseDouble(panelInput.getjTxtPriceNight().getText())));
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"El campo precio esta vacio");
-		}
-		
-		if(!panelInput.getjCbxStatusRoom().getSelectedItem().equals("Selecciona un estado de habitación")) {
-			room.setStatusRoom((String) panelInput.getjCbxStatusRoom().getSelectedItem());
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"No ha seleccionado algun estado");
-		}
-
-		if(!panelInput.getjCbxRoomType().getSelectedItem().equals("Seleccione un tipo de habitacion")) {
-			room.setRoomType((String) panelInput.getjCbxRoomType().getSelectedItem());
-			
-		}else {
-			JOptionPane.showMessageDialog(panelInput,"No ha seleccionado un tipo");
-		}
-		
-			
-		if(insertRoom(room)){
-			JOptionPane.showMessageDialog(panelInput, "Se han registrado los datos correctamente.");
-		}else {
-			JOptionPane.showConfirmDialog(panelInput, "No se han registrado los datos.");
-		}
-		
-		this.panelInput.cleanInputs();
-		this.panelInput.disableInputs();
-		showTable("");
 		
 	}
 	
@@ -109,12 +37,10 @@ public class QuerysRoom implements ActionListener{
 		modelTable=new DefaultTableModel(null, arrayColumnTitle);
 		
 		querySql="SELECT * FROM room WHERE floor LIKE '%"+search+"%' ORDER BY idRoom";
-		//querySql="SELECT * FROM room ORDER BY idRoom";
-		
+				
 		try {
 			
 			Statement myStatement=cn.createStatement();
-			//ResultSet myRs=myStatement.executeQuery(search);
 			ResultSet myRs=myStatement.executeQuery(querySql);
 			
 			while(myRs.next()) {
@@ -152,16 +78,19 @@ public class QuerysRoom implements ActionListener{
 			pst.setString(6, room.getStatusRoom());
 			pst.setString(7, room.getRoomType());
 			
-			
 			int n=pst.executeUpdate();
-			showTable("");
+			//showTable("");
 			
 			if(n!=0) {
+				//pst.close();
+				//cn.close();
 				return true;
+				
 			}else {
+				//pst.close();
+				//cn.close();
 				return false;
 			}
-			
 		}
 		catch(Exception e) {
 			JOptionPane.showConfirmDialog(null, e);
@@ -187,10 +116,11 @@ public class QuerysRoom implements ActionListener{
 			pst.setString(7, room.getRoomType());
 			pst.setInt(8, room.getIdRoom());
 			
-			
 			int n=pst.executeUpdate();
 			
 			if(n!=0) {
+				//pst.close();
+				//cn.close();
 				return true;
 			}else {
 				return false;
